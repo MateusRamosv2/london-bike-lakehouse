505da45acc23d14c48a778ae14d90a2d13b75f13
MateusRamosv2 <MateusRamosv2>
2025-08-27 19:58:35 +0000

    Feat(bronze): ingestão inicial dos dados crus

--- projeto_bikes/bronze/api_1_bronze.py
+++ projeto_bikes/bronze/api_1_bronze.py
@@ -5,3 +5,21 @@
 # ============================================
 
 
+# 1. Imports
+import pandas as pd
+
+# 2. Configurações iniciais
+caminho_arquivo = "/Volumes/workspace/default/arquivos-projetos/london_merged.csv"
+
+# 3. Leitura dos dados crus
+df_bronze = pd.read_csv(caminho_arquivo)
+
+# 4. Exibir amostra dos dados crus
+print("Dados crus - camada Bronze")
+display(df_bronze.head())
+
+# 5. (Opcional) salvar em formato parquet para futuras camadas
+# df_bronze.to_parquet("/Volumes/workspace/default/bronze/london_bike_bronze.parquet", index=False)
+
+
+
