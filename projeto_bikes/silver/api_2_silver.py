7104cf4fa9bc8a28a096d3dcb60ea4d025711552
MateusRamosv2 <MateusRamosv2>
2025-08-28 19:59:08 +0000

    feat(silver): transformação e enriquecimento dos dados brutos em dataset tratado

--- projeto_bikes/silver/api_2_silver.py
+++ projeto_bikes/silver/api_2_silver.py
@@ -1,2 +1,41 @@
 # Databricks notebook source
+# ============================================
+# CAMADA SILVER
+# Objetivo: Limpeza, tratamento e enriquecimento
+# ============================================
+
+
+import pandas as pd
+
+caminho_bronze = "/Volumes/Workspace/default/arquivos-projetos/london_merged.csv"
+df = pd.read_csv(caminho_bronze)
+
+
+df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+
+df["year"] = df["timestamp"].dt.year
+df["month"] = df["timestamp"].dt.month
+df["day"] = df["timestamp"].dt.day
+df["hour"] = df["timestamp"].dt.hour
+df["weekday"] = df["timestamp"].dt.weekday
+
+df = df.rename(columns={
+    "cnt" : "qtd_bikes",
+    "t1" : "temp_real",
+    "t2" : "temp_percebida",
+    "hum" : "umidade",
+    "wind_speed" : "velocidade_vento",
+    "weather_code" : "codigo_clima",
+    "is_holiday" : "feriado",
+    "is_weekend" : "final_semana",
+    "season" : "estacao"
+ })
+
+print(df.head())
+
+display(df)
+
+# 6. (Opcional) salvar dataset tratado
+# df.to_parquet("/Volumes/workspace/default/silver/london_bike_silver.parquet", index=False)
 
